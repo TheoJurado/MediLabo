910d5831ae32775f40df6a7cbf7593aa6de33b69
TheoJ <jurado.t.pro@gmail.com>
2025-04-13 20:50:43 +0200

    Add project files.

---
+++ MediLabo/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MediLabo.Models;
+
+namespace MediLabo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Users> Utilisateurs { get; set; }
+    }
+}
---
+++ MediLabo/Models/Users.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MediLabo.Models
+{
+    public class Users : IdentityUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public DateTime BirthDay { get; set; }
+        public GenderType Gender { get; set; } = GenderType.Other;
+        public string Adress { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+    }
+
+    public enum GenderType
+    {
+        Man,
+        Woman,
+        Other
+    }
+}
